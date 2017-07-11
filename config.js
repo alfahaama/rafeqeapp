0beea52c0c7c608dfdde67bfbca02dcfbe465d0e
Fahad Alsohami <famwadei@gmail.com>
2017-07-11 07:24:10 +0300

    Add files via upload

---
+++ config.js
@@ -0,0 +1,11 @@
+module.exports = {
+
+    FB_PAGE_TOKEN: 'EAAYUERUtBKwBALLgTBDXapHbyhZBdnbgXOe2ycbyRVwdHf6bLn330BKhkCV5JUNbcDezrjOaqVVZBpkiwbqBfLZCSAsTSvwXwvnfZBON88f3mkwLEdhMAC5blhuhGZAhASFZAYuK4Rk2oBt5y1jlHNDNRXm7KHUr2bMCWXOj4hzgZDZD',
+    FB_VERIFY_TOKEN: 'my_voice_and_my_password_ud_co_bot1',
+    API_AI_CLIENT_ACCESS_TOKEN: '57341875159b48c7921422a4ccbfd0a5',
+    FB_APP_SECRET: 'c2274fcb80a4a22c51a5b6051eea791b',
+    SERVER_URL: "https://rmedical.herokuapp.com/",
+    /*SMOOCH_APP_TOKEN: 'cvdlmicf87zzoq7h5kv2nwgk9',
+    SMOOCH_KEY_ID: '5944d75b67634a4b00891871',
+    SMOOCH_SECRET:'7NZ_Dw62MZZ6xaw8He7oZD2Z'*/
+};
